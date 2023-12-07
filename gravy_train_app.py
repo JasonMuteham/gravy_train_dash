615cbd6fc506b92f4c54e7845abb0d206786ee76
ancientwrangler <ancientwrangler@gmail.com>
2023-12-07 18:48:08 +0000

    New text

--- gravy_train_app.py
+++ gravy_train_app.py
@@ -32,7 +32,7 @@ def get_financial_year(yr):
     fin_yr = int(yr[2:])
     return f"{fin_yr}_{fin_yr+1}"
 
-st.sidebar.header("GravyTrain `V1.0.0`")
+st.sidebar.header("GravyTrain `V1.0.1`")
 selected_year = st.sidebar.number_input('Choose a year to view', min_value=2010, max_value=2023, value=selected_year, step=1, help="The financial year runs from 1 April to 31 March")
 
 financial_year = get_financial_year(str(selected_year))
@@ -139,6 +139,18 @@ with tab1:
 with tab2:
     """
 ### A geospatial visualisation of MP expense claims in the UK by constituency.
+
+MP expenses are a hot topic in the UK, and my MP raised eyebrows with large claims for rented accommodation, even though they live in a constituency close to Parliament. This made me wonder if the outrage was justified.
+
+- Would combining travel and accommodation expenses provide a fair picture?
+
+- How do MPs in distant constituencies compare? 
+
+- Is travelling from Scotland and staying in hotels comparable to permanently renting a home in London?
+
+- Should we assess costs per mile instead of total expenses?
+
+The outcome? The Gravy Train. Now armed with data, the question is: How does your MP's spending compare?
 
 Data is supplied by 
 
